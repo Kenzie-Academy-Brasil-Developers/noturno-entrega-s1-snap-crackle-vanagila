02071740ff7fb0d68c017a1e8fe5bddcafae517b
vanagila <vanagilakedna@gmail.com>
2021-10-20 22:56:49 -0300

    Add Resolução SnapCrackle

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,17 @@
-// Seu código vai aqui
+function snapCrackle(maxValue) {
+  let numero = [];
+
+  for (let counter = 1; numero.length < maxValue; counter++) {
+    if (counter % 2 !== 0 && counter % 5 === 0) {
+      numero.push("SnapCrackle");
+    } else if (counter % 2 !== 0) {
+      numero.push("Snap");
+    } else if (counter % 5 === 0) {
+      numero.push("Crackle");
+    } else {
+      numero.push(counter);
+    }
+  }
+  return numero;
+}
+console.log(snapCrackle(12));
